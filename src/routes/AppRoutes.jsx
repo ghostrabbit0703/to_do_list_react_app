0bfbccb25d86ea7519dbcbc874b9584b8228c7e8
ghostrabbit0703 <117048503+ghostrabbit0703@users.noreply.github.com>
2026-09-08 06:35:36 -0400

    Tod 17 login (#17)

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,6 +2,8 @@ import { Navigate, Route, Routes } from "react-router-dom";
 
 import MainLayout from "../components/layout/MainLayout";
 
+import LoginPage from "../pages/LoginPage";
+import RegisterPage from "../pages/RegisterPage";
 import CategoriesPage from "../pages/CategoriesPage";
 import TagsPage from "../pages/TagsPage";
 import TasksPage from "../pages/TaskPage";
@@ -11,6 +13,16 @@ function AppRoutes() {
     return (
         <Routes>
 
+            <Route
+                path={API_ENDPOINTS.AUTH.LOGIN}
+                element={<LoginPage />}
+            />
+
+            <Route
+                path={API_ENDPOINTS.AUTH.REGISTER}
+                element={<RegisterPage />}
+            />
+
             <Route element={<MainLayout />}>
 
                 <Route
@@ -19,7 +31,7 @@ function AppRoutes() {
                 />
 
                 <Route
-                    path={API_ENDPOINTS.TAGS.GET_ALL}
+                    path={API_ENDPOINTS.TAGS.GET_ALL}   
                     element={<TagsPage />}
                 />
 
@@ -32,7 +44,7 @@ function AppRoutes() {
 
             <Route
                 path="*"
-                element={<Navigate to={API_ENDPOINTS.CATEGORIES.GET_ALL} replace />}
+                element={<Navigate to={API_ENDPOINTS.AUTH.LOGIN} replace />}
             />
 
         </Routes>
